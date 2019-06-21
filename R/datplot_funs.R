7cae2f3664a11c752eacb81d448cab3416cc71f0
Lisa Steinmann <37840007+lsteinmann@users.noreply.github.com>
2019-06-21 15:18:53 +0200

    added options to scaleweight: either scaled along group membership of specified column index or scaled across all objects

--- R/datplot_funs.R
+++ R/datplot_funs.R
@@ -51,19 +51,24 @@ datsteps <- function(df, stepsize = 5) {
 #' Requires a dataframe as produced by datsteps(). (Meaning 6 columns in the following order: ID, group, minimum/earliest date, maximum/latest date, weight, 'DAT_Steps')
 #'
 #' @param df a dataframe as returned by datsteps
-#' @param var the column of said dataframe that should be used as the group variable
+#' @param var the index of the column of said dataframe that should be used as the group variable, OR "all" (note: all non-numeric values will result in the weight being scaled accross all objects)
 #'
-#' @return the same dataframe, with the 'weight'-values scaled along group membership
+#' @return the same dataframe, with scaled 'weight'-values
 #'
 #' @export scaleweight
 
-scaleweight <- function(df, var) {
+scaleweight <- function(df, var = c("all", 2) ) {
   res_df <- data.frame(NULL)
-  uvar <- unique(var)
-  for (row in 1:length(uvar)) {
-    wip <- df[which(var == uvar[row]),]
-    wip$weight <- wip$weight / sum(wip$weight)
-    res_df <- rbind(res_df, wip)
+  if (is.numeric(var)) {
+    uvar <- unique(df[,var])
+    for (row in 1:length(uvar)) {
+      wip <- df[which(df[,var] == uvar[row]),]
+      wip$weight <- wip$weight / sum(wip$weight)
+      res_df <- rbind(res_df, wip)
+    }
+  } else {
+    df$weight <- df$weight / sum(df$weight)
+    res_df <- df
   }
   return(res_df)
 }
